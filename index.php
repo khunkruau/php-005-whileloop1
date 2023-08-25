af1f3cb85a8d06eec83fddb0e31554ae393d8254
khunkruau <137683680+khunkruau@users.noreply.github.com>
2023-08-25 11:56:55 +0700

    update 25-08-2566

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>การทำซ้ำด้วย loop คำสั่ง while</title>
+    </head>
+    <body>
+        <?php
+                $i = 1;
+                while ($i<=10) {
+                    echo($i."<br>");
+                    $i++;
+                }
+        ?>
+    </body>
+</html>
